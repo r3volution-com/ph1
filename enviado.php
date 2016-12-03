89e38bbecb8bb2ea91a94c53dafe293b52483bf1
mario <mario@localhost.localdomain>
2016-12-03 21:39:07 +0100

    mas

--- enviado.php
+++ enviado.php
@@ -6,26 +6,29 @@
 		header("location: index.php");
 		exit;
 	}
-	if(!isset($_POST["nombre"])){
+	if(!isset($_POST["nombre"]) ||!isset($_POST["titulo"]) || !isset($_POST["adicional"]) || !isset($_POST["email"]) ||
+	!isset($_POST["direcion"]) || !isset($_POST["numero"]) ||  !isset($_POST["cod"]) || !isset($_POST["localidad"]) ||
+	!isset($_POST["prov"]) || !isset($_POST["pais"]) || !isset($_POST["color"]) || !isset($_POST["res"]) ||
+	!isset($_POST["numcopias"]) || !isset($_POST["album"]) || !isset($_POST["date"]) || !isset($_POST["colored"])){
 		header("location: index.php");
 		exit;
 	}
-	$nombre = $_POST["nombre"];
-	$titulo = $_POST["titulo"];
-	$adicional = $_POST["adicional"];
-	$email = $_POST["email"];
-	$direccion = $_POST["direccion"];
-	$numero = $_POST["numero"];
-	$cod = $_POST["cod"];
-	$localidad = $_POST["localidad"];
-	$prov = $_POST["prov"];
-	$pais = $_POST["pais"];
-	$color = $_POST["color"];
-	$res = $_POST["res"];
-	$numcopias = $_POST["numcopias"];
-	$album = $_POST["album"];
-	$date = $_POST["date"];
-	$colored = $_POST["colored"];
+	$nombre = htmlentities($db->real_escape_string($_POST["nombre"]));
+	$titulo = htmlentities($db->real_escape_string($_POST["titulo"]));
+	$adicional = htmlentities($db->real_escape_string($_POST["adicional"]));
+	$email = $db->real_escape_string($_POST["email"]);
+	$direccion = htmlentities($db->real_escape_string($_POST["direccion"]));
+	$numero = $db->real_escape_string($_POST["numero"]);
+	$cod = $db->real_escape_string($_POST["cod"]);
+	$localidad = htmlentities($db->real_escape_string($_POST["localidad"]));
+	$prov = htmlentities($db->real_escape_string($_POST["prov"]));
+	$pais = $db->real_escape_string($_POST["pais"]);
+	$color = $db->real_escape_string($_POST["color"]);
+	$res = $db->real_escape_string($_POST["res"]);
+	$numcopias = $db->real_escape_string($_POST["numcopias"]);
+	$album = $db->real_escape_string($_POST["album"]);
+	$date = $db->real_escape_string($_POST["date"]);
+	$colored = $db->real_escape_string($_POST["colored"]);
 	$error = false;
 	include("includes/header.php");
 ?>
@@ -48,33 +51,36 @@
 			echo"Se ha excedido el tamaño máximo de campo <b>Email</b>";
 			$error=true;
 		}
+	} else {
+		echo"El <b>Email</b> introducido no es valido";
+		$error=true;
 	}
 	if(strlen($direccion)>21){
 		echo"Se ha excedido el tamaño máximo de campo <b>Dirección</b>";
 		$error=true;
 	}
-	if(strlen($numero)>10){
-		echo"Se ha excedido el tamaño máximo de campo <b>Número</b>";
+	if(strlen($numero)>10 || !is_numeric($numero)){
+		echo"Se ha excedido el tamaño máximo o no es valido el campo <b>Número</b>";
 		$error=true;
 	}
 	if(strlen($cod)>6){
 		echo"Se ha excedido el tamaño máximo de campo <b>Código postal</b>";
 		$error=true;
 	}
-	if(strlen($localidad)>11){
+	if(strlen($localidad)>20){
 		echo"Se ha excedido el tamaño máximo de campo <b>Localidad</b>";
 		$error=true;
 	}
-	if(strlen($prov)>11){
+	if(strlen($prov)>20){
 		echo"Se ha excedido el tamaño máximo de campo <b>Provincia</b>";
 		$error=true;
 	}
-	if(strlen($pais)>11){
+	if(strlen($pais)>2 || !is_numeric($pais)){
 		echo"Se ha excedido el tamaño máximo de campo <b>País</b>";
 		$error=true;
 	}
 	if($error == false){
-		$db->query("INSERT INTO fotos (titulo, idAlbum, fecha, idPais, ruta) VALUES ('".$titulo."', ".$album.", '".$date."', ".$pais.", '".$ruta."')");
+		$db->query("INSERT INTO solicitudes (AQUI_ISMA) VALUES ('".$titulo."', ".$album.", '".$date."', ".$pais.", '".$ruta."')");
 ?>
 <h1>Pedido realizado</h1>
 	<p>Su pedido de envío de album impreso ha sido registrado. El coste es de <em><?php echo (($res/100)*$numcopias)+$colored; ?>€</em></p>
--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,7 @@
 session_start();
 include("includes/config.php");
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (!$db) die("ERROR CRITICO: NO HAY CONEXION CON LA BASE DE DATOS");
 $db->set_charset("utf8");
 ?>
 <!DOCTYPE html>
@@ -12,7 +13,7 @@ $db->set_charset("utf8");
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css"/>
         <link href="css/style.css" rel="stylesheet" type="text/css" media="screen"/>
         <link href="css/style-print.css" rel="stylesheet" type="text/css" media="print"/>
-		<link href="css/style-accesible.css" rel="alternate stylesheet" type="text/css" title="Accesible"/> 
+		<link href="css/style-accesible.css" rel="alternate stylesheet" type="text/css" title="Accesible"/>
 		<link href="css/<?php echo $cssfile; ?>.css" rel="stylesheet" type="text/css" media="screen"/>
 		<link href="css/<?php echo $cssfile; ?>-print.css" rel="stylesheet" type="text/css" media="print"/>
 	</head>
