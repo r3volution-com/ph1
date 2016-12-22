5fec6754395fe11d9d8a6c472133ff36df2a546f
ilygan <ismaismael006@hotmail.com>
2016-12-22 14:46:56 +0100

    atom rss

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 <footer>
 	WePic ©2016
+	<a class="rss" href="feed.php?type=rss"> <img src="images/rss.png"> </a>
+	<a class="atom" href="feed.php?type=atom"> <img src="images/atom.png"> </a>
 </footer>
